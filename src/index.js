bc4ae57691508b3dde8fae5cf57b7c30856f2f90
andreynho2006 <andreynho2006@gmail.com>
2019-04-22 11:07:49 +0300

    add a new action to REMOVE a day with date as payload

--- src/index.js
+++ src/index.js
@@ -7,17 +7,18 @@ const state = [
         "date": "2016-12-15",
         "powder" : true,
         "backcountry": false
+    },
+    {
+        "resort": "Boreal",
+        "date": "2016-12-16",
+        "powder": false,
+        "backcountry": false
     }
 ];
 
 const action = {
-    type: C.ADD_DAY,
-    payload: {
-        "resort": "Boreal",
-        "date": "2016-12-15",
-        "powder": false,
-        "backcountry": false
-    }
+    type: C.REMOVE_DAY,
+    payload: "2016-12-16"
 }
 
 const nextState = allSkiDays(state, action)
