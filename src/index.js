f103a1b8ca7bfe739965813be80d32ae84fe8efc
andreynho2006 <andreynho2006@gmail.com>
2019-04-22 10:30:28 +0300

    set initial state, an actuion with a type and a data, and call a reducer to get a new state

--- src/index.js
+++ src/index.js
@@ -1,16 +1,36 @@
 import C from './constants';
-import { allSkyDays, goal } from './initialState.json';
+import { goal } from './store/reducers';
+
+const state = 10;
+
+const action = {
+    type: C.SET_GOAL,
+    payload: 15
+}
+
+const nextState = goal(state, action)
 
 console.log(`
+        
+    initial goal: ${state}
+    action: ${JSON.stringify(action)}
+    new goal: ${nextState}
 
-    Sky Day Counter
-    ===================
-    The goal is ${goal} days
-    Initial there are ${allSkyDays.length} sky days in state
-
-    
-    Constants (actions)
-    ===================
-    ${Object.keys(C).join('\n     ')}
-    
 `)
+
+
+
+
+// console.log(`
+
+//     Sky Day Counter
+//     ===================
+//     The goal is ${goal} days
+//     Initial there are ${allSkyDays.length} sky days in state
+
+    
+//     Constants (actions)
+//     ===================
+//     ${Object.keys(C).join('\n     ')}
+
+// `)
