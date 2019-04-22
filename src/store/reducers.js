7c316e6b8ca8e02a5ccf96120d860fda0d42ec29
andreynho2006 <andreynho2006@gmail.com>
2019-04-22 10:37:07 +0300

    add a skiDay reducer

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -9,3 +9,10 @@ export const goal = (state, action) => {
     }
 }
 
+export const skiDay = (state=null, action) => {
+    if(action.type ===C.ADD_DAY) {
+        return action.payload
+    } else {
+        return state
+    }
+}
