6f021c75265cf6fe5d5181650fdd3edb3186750d
andreynho2006 <andreynho2006@gmail.com>
2019-04-22 13:05:10 +0300

    create store, console.log getstate() of the store, dispatch an action and console.log getstate of the store

--- src/index.js
+++ src/index.js
@@ -1,45 +1,20 @@
 import C from './constants';
 import appReducer from './store/reducers';
 import initialState from './initialState.json'
+import { createStore } from 'redux';
 
-let state = initialState;
+const store = createStore(appReducer, initialState);
 
-console.log(`
+console.log('initial state', store.getState());
 
-    Initial State
-    ==========
-    goal: ${state.goal}
-    resorts: ${JSON.stringify(state.allSkiDays)}
-    fetching: ${state.resortNames.fetching}
-    suggestions: ${state.resortNames.suggestions}
-`)
-
-state = appReducer(state, {
-    type: C.SET_GOAL,
-    payload: 2
-})
-
-state = appReducer(state, {
+store.dispatch({
     type: C.ADD_DAY,
     payload: {
         "resort": "Mt Shasta",
         "date":"2016-10-28",
         "powder": false,
-        "backcountr": true
+        "backcountry": true
     }
-})
+});
 
-state = appReducer(state, {
-    type: C.CHANGE_SUGGESTIONS,
-    payload: ["MT Tallac", "Mt Shasta"]
-})
-
-console.log(`
-
-    Next State
-    ==========
-    goal: ${state.goal}
-    resorts: ${JSON.stringify(state.allSkiDays)}
-    fetching: ${state.resortNames.fetching}
-    suggestions: ${state.resortNames.suggestions}
-`)
+console.log('next state', store.getState());
